fd8cc139a2d760cb1b47478264e606ef23cebed3
Gabriele Garofalo <gabrielegarofalo999@gmail.com>
2023-04-05 00:35:11 +0200

    Dictionary keys bug solved

--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -10,18 +10,16 @@ public class GridManager : MonoBehaviour
     public Candy CandyPrefab;
     public int MaxRow;
     public int MaxColumn;
-    public Dictionary<int[], Tile> MapTiles = new Dictionary<int[], Tile>();
+    public Tile[,] Tiles;
+    public static Tile[] PressedTiles;
 
     private GridLayoutGroup m_gridData;
-    private int m_lastColor, m_secondLastColor;
 
-    public static Tile[] PressedTiles;
 
     private void Awake()
     {
         PressedTiles = new Tile[2];
-        m_lastColor = -1;
-        m_secondLastColor = -1;
+        Tiles = new Tile[MaxRow, MaxColumn];
         InitializeGridData();
         
     }
@@ -50,9 +48,7 @@ public class GridManager : MonoBehaviour
                 candy.Initialize(0, 0, candyColor, candy.CandySprites[(int)candyColor]);
 
                 tile.name = "Tile - (" + row.ToString() + " - " + column.ToString() + ")";
-                int[] newKeyMap = { row, column };
-                //MapTiles[newKeyMap] = tile;
-                MapTiles.Add(newKeyMap, tile);
+                Tiles[row, column] = tile;
             }
         }
     }
@@ -68,92 +64,52 @@ public class GridManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Prevents three tiles of the same color from spawning one after the other horizontally
+    /// Prevents three tiles of the same color from spawning one after the other horizontally or vertically
     /// </summary>
-    /// <param name="color"></param>
+    /// <param name="color">color to check</param>
+    /// <param name="y">row of the candy</param>
+    /// <param name="x">column of the candy</param>
     /// <returns></returns>
-    private CandyColor CheckColorAdmissibility(int color)
-    {
-        if (m_lastColor == -1)
-        {
-            m_lastColor = color;
-            return (CandyColor)color;
-        }
-        if (m_secondLastColor == -1)
-        {
-            m_secondLastColor = color;
-            return (CandyColor)color;
-        }
-
-
-        if (m_lastColor == m_secondLastColor)
-        {
-            while (color == m_lastColor) color = RandomColor();
-
-            m_secondLastColor = m_lastColor;
-            m_lastColor = color;
-            return (CandyColor)color;
-        }
-
-
-        m_secondLastColor = m_lastColor;
-        m_lastColor = color;
-        return (CandyColor)color;
-    }
-
-    public static int RandomColor()
-    {
-        return Random.Range(0, 4);
-    }
-
-    /// <summary>
-    /// Return randomly 1 or -1;
-    /// </summary>
-    /// <returns></returns>
-    public static int RandomSign()
-    {
-        if (Random.value > 0.5) return 1;
-        else return -1;
-    }
-
     private CandyColor CheckColor(int color, int y, int x)
     {
-        bool checkVertical = false;
-        bool checkHorizontal = false;
+        bool checkVertical = false; //must check if there are combinations of three candies vertically?
+        bool checkHorizontal = false; //must check if there are combinations of three candies horizontally?
 
+        //coordinates of the positions to be checked
         int[] verticalKeyMapOne = { y-1, x };
         int[] verticalKeyMapTwo = { y-2, x };
         int[] horizontalKeyMapOne = { y, x-1 };
         int[] horizontalKeyMapTwo = { y, x-2 };
 
+        //where to store the candies if it exists in the positions listed above
         Candy onRightSideOne = null;
         Candy onRightSideTwo = null;
         Candy onBelowOne = null;
         Candy onBelowTwo = null;
 
-        if (MapTiles.TryGetValue(verticalKeyMapTwo, out Tile tile1) == true)
+        if (y >= 2 && Tiles[verticalKeyMapTwo[0], verticalKeyMapTwo[1]] != null) //if they exists stores the candies along vertically
         {
-            onBelowOne = MapTiles[verticalKeyMapOne].GetComponentInChildren<Candy>();
-            onBelowTwo = MapTiles[verticalKeyMapTwo].GetComponentInChildren<Candy>();
+            onBelowOne = Tiles[verticalKeyMapOne[0], verticalKeyMapOne[1]].GetComponentInChildren<Candy>();
+            onBelowTwo = Tiles[verticalKeyMapTwo[0], verticalKeyMapTwo[1]].GetComponentInChildren<Candy>();
             checkVertical = true;
         }
-        if (MapTiles.TryGetValue(horizontalKeyMapTwo, out Tile tile2) == true)
+        if (x >= 2 && Tiles[horizontalKeyMapTwo[0], horizontalKeyMapTwo[1]] != null) //if they exists stores the candies along horizontally
         {
-            onRightSideOne = MapTiles[horizontalKeyMapOne].GetComponentInChildren<Candy>();
-            onRightSideTwo = MapTiles[horizontalKeyMapTwo].GetComponentInChildren<Candy>();
+            onRightSideOne = Tiles[horizontalKeyMapOne[0], horizontalKeyMapOne[1]].GetComponentInChildren<Candy>();
+            onRightSideTwo = Tiles[horizontalKeyMapTwo[0], horizontalKeyMapTwo[1]].GetComponentInChildren<Candy>();
             checkHorizontal = true;
         }
 
 
         int notAvailableColor = -1;
 
-        if (checkHorizontal && onRightSideOne.Data.candyColor == onRightSideTwo.Data.candyColor)
+        if (checkHorizontal && onRightSideOne.Data.candyColor == onRightSideTwo.Data.candyColor) //check if the colors of the stored candies are equals to the current candy color to analyze (Horizontally)
         {
             notAvailableColor = (int)onRightSideOne.Data.candyColor;
             while (color == notAvailableColor) color = RandomColor();
         }
 
-        if (checkVertical && onBelowOne.Data.candyColor == onBelowTwo.Data.candyColor)
+        if (checkVertical && onBelowOne.Data.candyColor == onBelowTwo.Data.candyColor) //check if the colors of the stored candies are equals to the current candy color to analyze (Vertically)
         {
             while (color == (int)onBelowOne.Data.candyColor || color == notAvailableColor) color = RandomColor();
         }
@@ -161,8 +117,59 @@ public class GridManager : MonoBehaviour
         return (CandyColor)color;
     }
 
+    /// <summary>
+    /// </summary>
+    /// <returns>random integer between 0 and 3 inclusive</returns>
+    public static int RandomColor()
+    {
+        return Random.Range(0, 4);
+    }
+
+    /// <summary>
+    /// </summary>
+    /// <returns>1 or -1 randomly</returns>
+    public static int RandomSign()
+    {
+        if (Random.value > 0.5) return 1;
+        else return -1;
+    }
+
+
+
 }
 
 
 
+/// <summary>
+/// Prevents three tiles of the same color from spawning one after the other horizontally or vertically
+/// </summary>
+/// <param name="color"></param>
+/// <returns></returns>
+//private CandyColor CheckColorAdmissibility(int color)
+//{
+//    if (m_lastColor == -1)
+//    {
+//        m_lastColor = color;
+//        return (CandyColor)color;
+//    }
+//    if (m_secondLastColor == -1)
+//    {
+//        m_secondLastColor = color;
+//        return (CandyColor)color;
+//    }
 
+
+//    if (m_lastColor == m_secondLastColor)
+//    {
+//        while (color == m_lastColor) color = RandomColor();
+
+//        m_secondLastColor = m_lastColor;
+//        m_lastColor = color;
+//        return (CandyColor)color;
+//    }
+
+
+//    m_secondLastColor = m_lastColor;
+//    m_lastColor = color;
+//    return (CandyColor)color;
+//}
